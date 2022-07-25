088e24f34b5353e9a15301e106907c3da70ad2dd
Matt Dupree <kmdupr33@gmail.com>
2022-07-25 15:48:40 -0400

    add message after restart that tells them to resume setup in data chimp app

--- R/add_token.R
+++ R/add_token.R
@@ -3,5 +3,5 @@
 add_token_to_renviron <- function(token) {
   path <- usethis:::scoped_path_r(c("user", "project"), ".Renviron", envvar = "R_ENVIRON_USER")
   write(paste0("CHIMP_TOKEN=", token), file = path, append = T)
-  rstudioapi::restartSession()
+  rstudioapi::restartSession(command = 'cat("Token added! Resume setup in the data chimp web app.")')
 }
